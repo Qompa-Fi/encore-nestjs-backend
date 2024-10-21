f7f32bbec013dc310a57cf1eca5dddba9f4a7b81
Luis Quiñones <lpaandres2020@gmail.com>
2024-10-21 12:11:27 -0600

    refactor(organizations): using recommended syntax to define API params and response

--- services/organizations/types/request.ts
+++ services/organizations/types/request.ts
@@ -1,3 +1,12 @@
+import type { TeamSize } from "./team-size";
+
 export interface GetUserOrganizationParams {
   id: number;
+}
+
+export interface CreateOrganizationParams {
+  name: string;
+  category: string;
+  ruc: string;
+  size: TeamSize;
 }
