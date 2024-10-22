7a8b2642d79867a47c5254c01830f93f892bbc30
Luis Quiñones <lpaandres2020@gmail.com>
2024-10-22 10:44:35 -0600

    fix(users): all parameters to update an user are optional

--- services/users/types/request.ts
+++ services/users/types/request.ts
@@ -4,13 +4,13 @@ export interface CreateUserParams {
   accept_terms_and_privacy_policy: boolean;
 }
 
-export type UpdateUserParams = {
-  accept_terms_and_privacy_policy: boolean;
+export interface UpdateUserParams {
+  accept_terms_and_privacy_policy?: boolean;
   document?: {
     type: DocumentType;
     number: string;
   };
-};
+}
 
 export interface ExistsByIDParams {
   id: number;
